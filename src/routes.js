8ec8fa643831f996678067c7b2384869f425d6a4
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-27 15:13:25 -0300

    Criando login de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  return res.json({ message: 'hello world' });
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
