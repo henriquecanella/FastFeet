2762f2c29ef0de1f4e49cee4a4b41c8bf57a799e
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-27 18:38:12 -0300

    Configurando upload de imagens

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -6,6 +9,7 @@ import authMiddleware from './app/middlewares/auth';
 import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
