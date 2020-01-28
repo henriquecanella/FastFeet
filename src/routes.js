106da8012425705c69d9ea7643c2979e51367b05
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-27 22:56:18 -0300

    Salvando imagens no banco de dados

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +18,6 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
