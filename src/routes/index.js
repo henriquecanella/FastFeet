b2a013a93e6b84fc62a1a640bcf68e3242fe8b48
Henrique Canella <henrique.mandelli@gmail.com>
2020-03-10 00:27:59 -0300

    Configurando rotas da aplicação

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+
+import Orders from '../pages/Orders';
+import Deliverymans from '../pages/Deliverymans';
+import Recipients from '../pages/Recipients';
+import Problems from '../pages/Problems';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/orders" component={Orders} isPrivate />
+      <Route path="/deliverymans" component={Deliverymans} isPrivate />
+      <Route path="/recipients" component={Recipients} isPrivate />
+      <Route path="/problems" component={Problems} isPrivate />
+    </Switch>
+  );
+}
